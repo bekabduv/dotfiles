696bbd7ed12bc72981bb5708f9c0c190066b05ef
bekabduv <bekabduv@gmail.com>
2026-05-20 20:08:48 +0500

    Auto-sync

---
+++ scripts/bin/astro-deps-setup.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+JSON_FILE="package.json"
+
+if [[ ! -f "$JSON_FILE" ]]; then
+  echo "You have to be at the root folder!"
+  exit 1
+fi
+
+VITE_FILE=$(ls vite.config.ts vite.config.js 2>/dev/null | head -1)
+# if [[ ! -f "$VITE_FILE" ]]; then
+#   echo "This should be a Vite project"
+#   exit 1
+# fi
+
+confirm() {
+  while true; do
+    read -rp "${1:-Continue?} (Y/n): " response
+    case "$response" in
+    [Yy])
+      return 0
+      ;;
+    [Nn])
+      return 1
+      ;;
+    *)
+      echo -ne "\r"
+      ;;
+    esac
+  done
+
+}
+
+PM=""
+PMX=""
+COMMAND=""
+
+if [[ -f "./bun.lock" ]] || [[ -f "./bun.lockb" ]]; then
+  PM="bun"
+  PMX="bunx"
+  COMMAND="add"
+elif [[ -f "./pnpm-lock.yaml" ]]; then
+  PM="pnpm"
+  PMX="pnpm dlx"
+  COMMAND="pnpm add"
+elif [[ -f "./package-lock.json" ]]; then
+  PM="npm"
+  PMX="npx"
+  COMMAND="npm install"
+elif [[ -f "./yarn.lock" ]]; then
+  PM="yarn"
+  PMX="yarn dlx"
+  COMMAND="yarn add"
+else
+  echo "Could Not Detect Package Manager"
+  exit 1
+fi
+
+if confirm "Add Solidjs"; then
+  $PMX astro add solid
+fi
+
+if confirm "Add tailwindcss"; then
+  $PMX astro add tailwind
+fi
+
+if confirm "Add Reactjs"; then
+  $PMX astro add react
+fi
+
+if confirm "Add sitemap.xml"; then
+  $PMX astro add sitemap
+fi
+
+# Simple libaries are installed like npm i [name]
+# but if i changes how the site runs, changes config files then npx astro add [name]
+
+if confirm "Add Lucide"; then
+  $PM $COMMAND @lucide/astro
+fi
